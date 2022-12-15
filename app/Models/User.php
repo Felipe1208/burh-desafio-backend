92e685ffe0dfb734172a16221b547e5924558288
Felipe Jorge <feraikou@hotmail.com>
2022-12-15 16:04:32 -0300

    Criando models

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'cpf',
+        'birth_date',
     ];
 
     /**
@@ -30,15 +32,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'deleted_at',
     ];
 }
